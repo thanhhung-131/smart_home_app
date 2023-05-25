84645c606b4784b95cd6ded016d4b1abd0c8b2fe
Daiki <hothanhhung131@gmail.com>
2023-05-25 22:39:24 +0700

    complete all

--- lib/views/smart_home_control_view.dart
+++ lib/views/smart_home_control_view.dart
@@ -11,7 +11,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_switch/flutter_switch.dart';
 
 // var url = 'http://192.168.0.104:8000/';
-var url = 'http://192.168.0.104:5000/';
+var url = 'http://192.168.43.235:5000/';
 
 Future<Widget> living(BuildContext context, String path) async {
   final uri = Uri.parse(url + path);
@@ -32,18 +32,6 @@ Future<Widget> bedroom(BuildContext context, String path) async {
 }
 
 Future<Widget> toilet(BuildContext context, String path) async {
-  final uri = Uri.parse(url + path);
-  await http.get(uri);
-  return const CircularProgressIndicator();
-}
-
-Future<Widget> fankitchen(BuildContext context, String path) async {
-  final uri = Uri.parse(url + path);
-  await http.get(uri);
-  return const CircularProgressIndicator();
-}
-
-Future<Widget> fanbedroom(BuildContext context, String path) async {
   final uri = Uri.parse(url + path);
   await http.get(uri);
   return const CircularProgressIndicator();
@@ -70,72 +58,68 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
   bool _gara = false;
   bool _bedrom = false;
   bool _toilet = false;
+  bool _all = false;
 
-  // Future<String> getTemp() async {
-  //   String nhietdo = 'temperature: ';
-  //   var client = HttpClient();
-  //   var uri = Uri.parse(url);
-  //   var request = await client.getUrl(uri);
-  //   var response = await request.close();
-  //   var responseBody = await response.transform(utf8.decoder).join();
-  //   var start = responseBody.indexOf(nhietdo, 0);
-  //   var end = responseBody.indexOf('\'C');
-  //   String lt = responseBody.substring(start + nhietdo.length, end);
-  //   return lt;
-  // }
+  Future<String> getTemp() async {
+    String nhietdo = 'temperature: ';
+    var client = HttpClient();
+    var uri = Uri.parse(url);
+    var request = await client.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(utf8.decoder).join();
+    var start = responseBody.indexOf(nhietdo, 0);
+    var end = responseBody.indexOf('\'C');
+    String lt = responseBody.substring(start + nhietdo.length, end);
+    if (response.statusCode == 200) {
+      setState(() {
+        lt = responseBody.substring(start + nhietdo.length, end);
+      });
+    }
+    return lt;
+  }
 
-  // Future<String> getHumid() async {
-  //   String doam = 'humidity: ';
-  //   var client = HttpClient();
-  //   var uri = Uri.parse(url);
-  //   var request = await client.getUrl(uri);
-  //   var response = await request.close();
-  //   var responseBody = await response.transform(utf8.decoder).join();
-  //   var start = responseBody.indexOf(doam, 0);
-  //   var end = responseBody.indexOf('%');
-  //   String lt = responseBody.substring(start + doam.length, end);
-  //   return lt;
-  // }
+  Future<String> getHumid() async {
+    String doam = 'humidity: ';
+    var client = HttpClient();
+    var uri = Uri.parse(url);
+    var request = await client.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(utf8.decoder).join();
+    var start = responseBody.indexOf(doam, 0);
+    var end = responseBody.indexOf('%');
+    String lt = responseBody.substring(start + doam.length, end);
+    return lt;
+  }
 
-  // Future<String> getGas() async {
-  //   String gas = 'gas: ';
-  //   var client = HttpClient();
-  //   var uri = Uri.parse(url);
-  //   var request = await client.getUrl(uri);
-  //   var response = await request.close();
-  //   var responseBody = await response.transform(utf8.decoder).join();
-  //   var start = responseBody.indexOf(gas, 0);
-  //   var end = responseBody.indexOf('.');
-  //   String lt = responseBody.substring(start + gas.length, end);
-  //   return lt;
-  // }
+  Future<String> getGas() async {
+    String gas = 'gas: ';
+    var client = HttpClient();
+    var uri = Uri.parse(url);
+    var request = await client.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(utf8.decoder).join();
+    var start = responseBody.indexOf(gas, 0);
+    var end = responseBody.indexOf('.');
+    String lt = responseBody.substring(start + gas.length, end);
+    return lt;
+  }
 
-  // Future<String> getLight() async {
-  //   String light = 'light: ';
-  //   var client = HttpClient();
-  //   var uri = Uri.parse(url);
-  //   var request = await client.getUrl(uri);
-  //   var response = await request.close();
-  //   var responseBody = await response.transform(utf8.decoder).join();
-  //   var start = responseBody.indexOf(light, 0);
-  //   var end = responseBody.indexOf(',');
-  //   String lt = responseBody.substring(start + light.length, end);
-  //   return lt;
-  // }
+  Future<String> getLight() async {
+    String light = 'light: ';
+    var client = HttpClient();
+    var uri = Uri.parse(url);
+    var request = await client.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(utf8.decoder).join();
+    var start = responseBody.indexOf(light, 0);
+    var end = responseBody.indexOf(',');
+    String lt = responseBody.substring(start + light.length, end);
+    return lt;
+  }
 
   // Timer timer = Timer(const Duration(seconds: 5), () {});
 
   Color c = const Color(0xFF42A5F5);
-
-  // @override
-  // void initialize() {
-  //   setState(() {
-  //     getTemp();
-  //     getHumid();
-  //     getLight();
-  //     getGas();
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -170,31 +154,106 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
         ),
         body: Column(children: [
           Container(
-            height: 200,
-            width: 320,
-            color: c,
-            margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-            child: GridView.count(
-              crossAxisCount: 2,
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  height: 5,
-                  color: Colors.white10,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  height: 5,
-                  color: Colors.white,
-                )
-              ],
-            ),
-          ),
+              margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+              height: 200,
+              width: 320,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const ImageIcon(
+                        AssetImage('assets/thermometer.png'),
+                        size: 40,
+                      ),
+                      FutureBuilder(
+                          future: getTemp(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> text) {
+                            if (text.hasData) {
+                              return Text("Temerature: ${text.data}℃");
+                            }
+                            if (text.hasError) {
+                              return Text(text.error.toString());
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const ImageIcon(
+                        AssetImage('assets/humidity.png'),
+                        size: 40,
+                      ),
+                      FutureBuilder(
+                          future: getHumid(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> text) {
+                            if (text.hasData) {
+                              return Text("Humidity: ${text.data}%");
+                            }
+                            if (text.hasError) {
+                              return Text(text.error.toString());
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const ImageIcon(
+                        AssetImage('assets/gas-bottle.png'),
+                        size: 40,
+                      ),
+                      FutureBuilder(
+                          future: getGas(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> text) {
+                            if (text.hasData) {
+                              return Text("Gas: ${text.data}");
+                            }
+                            if (text.hasError) {
+                              return Text(text.error.toString());
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const ImageIcon(
+                        AssetImage('assets/daytime.png'),
+                        size: 40,
+                      ),
+                      FutureBuilder(
+                          future: getLight(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> text) {
+                            if (text.hasData) {
+                              return Text("Time: ${text.data}");
+                            }
+                            if (text.hasError) {
+                              return Text(text.error.toString());
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          }),
+                    ],
+                  )
+                ],
+              )),
           Container(
             padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(6),
-              color: const Color.fromARGB(223, 95, 223, 218),
+              color: const Color.fromARGB(255, 250, 234, 177),
               boxShadow: const [
                 BoxShadow(color: Colors.white10, spreadRadius: 2),
               ],
@@ -211,17 +270,25 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
                   activeIcon: const Icon(Icons.light_mode),
                   borderRadius: 20.0,
                   activeColor: c,
-                  value: _toilet,
+                  value: _all,
                   onToggle: (value) {
                     if (value) {
                       setState(() {
-                        _toilet = value;
-                        toilet(context, 'toilet/on');
+                        _all = value;
+                        _living = true;
+                        _bedrom = true;
+                        _toilet = true;
+                        _gara = true;
+                        toilet(context, 'all/on');
                       });
                     } else {
                       setState(() {
-                        _toilet = value;
-                        toilet(context, 'toilet/off');
+                        _all = value;
+                        _living = false;
+                        _bedrom = false;
+                        _toilet = false;
+                        _gara = false;
+                        toilet(context, 'all/off');
                       });
                     }
                   },
@@ -239,7 +306,7 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
                     padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(6),
-                      color: const Color.fromARGB(223, 95, 223, 218),
+                      color: const Color.fromARGB(255, 250, 234, 177),
                       boxShadow: const [
                         BoxShadow(color: Colors.white10, spreadRadius: 2),
                       ],
@@ -276,12 +343,13 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
                           }
                         },
                       ),
+                      const Text('Living room'),
                     ])),
                 Container(
                     padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(6),
-                      color: const Color.fromARGB(223, 95, 223, 218),
+                      color: const Color.fromARGB(255, 250, 234, 177),
                       boxShadow: const [
                         BoxShadow(color: Colors.white10, spreadRadius: 2),
                       ],
@@ -324,7 +392,7 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
                     padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(6),
-                      color: const Color.fromARGB(223, 95, 223, 218),
+                      color: const Color.fromARGB(255, 250, 234, 177),
                       boxShadow: const [
                         BoxShadow(color: Colors.white10, spreadRadius: 2),
                       ],
@@ -367,7 +435,7 @@ class _SmartHomeControlsState extends State<SmartHomeControls> {
                     padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(6),
-                      color: const Color.fromARGB(223, 95, 223, 218),
+                      color: const Color.fromARGB(255, 250, 234, 177),
                       boxShadow: const [
                         BoxShadow(color: Colors.white10, spreadRadius: 2),
                       ],
